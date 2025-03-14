2aba22f000b6640fe478feee0b594f1a884b3407
Efran Huliselan <ee.huliselan@st.hanze.nl>
2025-03-14 19:40:20 +0100

    app.py aangepast met alle linkes naar de pagina's

--- app.py
+++ app.py
@@ -4,12 +4,20 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home_pagina():
+    return render_template('base.html')
 
 @app.route('/about')
 def dynamische_about_pagina():
     return render_template('About_page.html')
 
+@app.route('/tutorial')
+def tutorial_pagina():
+    return render_template('tutorial_page.html')
+
+@app.route('/contact')
+def contact_pagina():
+    return render_template('contact_page.html')
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)

cb65ef58b9987dae243dc47857a57c0677f095d8
Gea2000 <g.a.bakker@st.hanze.nl>
2025-03-25 09:21:41 +0100

    Made tests for unit testing

---
+++ Unit_testing_tests.py
@@ -0,0 +1,37 @@
+import unittest
+
+class test_sequences(unittest.TestCase):
+    def test_sequence_1(self):
+        found = (['ATGCCGAT'])
+        expected = (['ATGCCGAT']) == True
+        self.assertEqual(found, expected)
+
+    def test_sequence_2(self):
+        found = (['AUGCAUGC'])
+        expected = (['AUGCAUGC']) == True
+        self.assertEqual(found, expected)
+
+    def test_sequence_3(self):
+        found = (['AUGTAGUT'])
+        expected = (['AUGTAGUT']) == False
+        self.assertEqual(found, expected)
+
+    def test_sequence_4(self):
+        found = (['BJOXZBJOXZ'])
+        expected = (['BJOXZBJOXZ']) == False
+        self.assertEqual(found, expected)
+
+    def test_sequence_5(self):
+        found = (['ACDEFGH'])
+        expected = (['ACDEFGH']) == True
+        self.assertEqual(found, expected)
+
+    def test_sequence_6(self):
+        found = (['IKLMNP'])
+        expected = (['IKLMNP']) == True
+        self.assertEqual(found, expected)
+
+    def test_sequence_7(self):
+        found = (['QRSTVWY'])
+        expected = (['QRSTVWY']) == True
+        self.assertEqual(found, expected)

ee5b8a41b4b0ce92c313ae0b929427f0899ab5a6
Herke W. <herkewilts@outlook.com>
2025-04-02 09:20:46 +0200

    made it function again by changing self.

--- fasttree.py
+++ fasttree.py
@@ -3,12 +3,12 @@ import subprocess
 class FastTree:
     def __init__(self, file):
         self.file = file
+        self.output_file = "output.nw"
     
     def run_fasttree(self):
-        output_file = "output.nw"
         with open(self.output_file,"w") as out:
             subprocess.run(["FastTree", self.file], stdout=out, check=True)
-        print(f"FastTree finished. Output saved to {output_file}")
+        print(f"FastTree finished. Output saved to {self.output_file}")
 
     def __str__(self):
         return f"FastTree instance processing file: {self.file}"

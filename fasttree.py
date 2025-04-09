706dcf0151fc2a172f58f8b7d25361dcfdbdca2c
Herke W. <herkewilts@outlook.com>
2025-04-09 10:55:59 +0200

    Fixed the fasttree so the tool works with the new tool.

--- fasttree.py
+++ fasttree.py
@@ -1,19 +1,16 @@
 import subprocess
 
 class FastTree:
-    def __init__(self, file):
+    def __init__(self, file, output_file="output.nw", output_image="newick.png"):
         self.file = file
-        self.output_file = "output.nw"
-        self.output_newick = "newick.png"
-    
+        self.output_file = output_file
+        self.output_image = output_image
+
     def run_fasttree(self):
         with open(self.output_file, "w") as nw_out:
             subprocess.run(["FastTree", self.file], stdout=nw_out, check=True)
-        print(f"FastTree finished. Output saved to {self.output_file}")
-        
-    def run_ete_toolkit(self):
-        with open(self.output_newick, "w") as img_out:
-            subprocess.run(["/homes/hjwilts/ete_env/bin/ete3", "view", "-t", self.output_file], stdout=img_out, check=True)
-        
-    def __str__(self):
-        return f"FastTree instance processing file: {self.file}"
+
+    def render_tree_image(self):
+        subprocess.run(
+            ["python", "render_tree.py", self.output_file, self.output_image],
+            check=True)
